0b204063ccb87f0aca9c1891a887c90a2c5d3715
19983467897 <2715158815.com>
2025-04-17 01:02:10 +0800

    fix: optimize fastify https

--- src/utils/https.util.ts
+++ src/utils/https.util.ts
@@ -2,20 +2,24 @@ import { HttpsOptions } from '@nestjs/common/interfaces/external/https-options.i
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 
-export async function  loadHttpOptions(isDev): Promise<HttpsOptions> {
-  if(isDev) {
-    return null
+export async function loadHttpOptions(isDev): Promise<HttpsOptions> {
+  if (isDev) {
+    return null;
   }
 
-  const key = await fs.readFileSync(path.join(__dirname, '..', 'https', 'private-key.pem'));
-  const cert = await fs.readFileSync(path.join(__dirname, '..', 'https', 'public-certificate.pem'));
-  
-  if(!key.length || !cert.length) {
-    throw new Error('https key or cert not found');
+  const key = await fs.readFileSync(
+    path.join(__dirname, '..', 'https', 'private-key.pem'),
+  );
+  const cert = await fs.readFileSync(
+    path.join(__dirname, '..', 'https', 'public-certificate.pem'),
+  );
+
+  if (!key.length || !cert.length) {
+    console.error('https key or cert not found');
   }
 
   return {
     key,
     cert,
-  }
+  };
 }
