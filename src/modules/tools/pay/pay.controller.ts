45c998d048717651f01286da5ac598d5c348c9ba
xiaozhang <2715158815@qq.com>
2025-04-25 16:57:29 +0800

    feat: add SecurityAuth for pay

--- src/modules/tools/pay/pay.controller.ts
+++ src/modules/tools/pay/pay.controller.ts
@@ -4,7 +4,6 @@ import { ApiSecurityAuth } from '~/common/decorators/swagger.decorators';
 import { PayDto, PayRefundDto } from './Pay.dto';
 import { Throttle, ThrottlerGuard } from '@nestjs/throttler';
 import { PayService } from './pay.service';
-import { Public } from '~/modules/auth/decorators/public.decorator';
 import { ParamId } from '~/common/decorators/param-id.decorator';
 
 @ApiTags('Tools - 支付')
@@ -16,7 +15,6 @@ export class PayController {
 
   @Post('pay')
   @ApiOperation({ summary: '支付订单' })
-  @Public()
   @Throttle({ default: { limit: 5, ttl: 60 * 1000 } })
   async pay(@Body() dto: PayDto): Promise<string> {
     return await this.payService.pay(dto);
@@ -24,7 +22,6 @@ export class PayController {
 
   @Post('verify/:id')
   @ApiOperation({ summary: '校验订单是否支付' })
-  @Public()
   @Throttle({ default: { limit: 5, ttl: 60 * 1000 } })
   async verify(@ParamId() id: number): Promise<boolean> {
     return await this.payService.verify(id);
@@ -32,14 +29,12 @@ export class PayController {
 
   @Post('refund')
   @ApiOperation({ summary: '订单退款' })
-  @Public()
   async refund(@Body() dto: PayRefundDto): Promise<boolean> {
     return await this.payService.refund(dto);
   }
 
   @Post('close/:id')
   @ApiOperation({ summary: '关闭订单' })
-  @Public()
   async close(@ParamId() id: number): Promise<boolean> {
     return await this.payService.close(id);
   }
