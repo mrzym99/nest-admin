057da0da70abc95c717fab6890e81764a6f1fb0a
19983467897 <2715158815.com>
2025-05-27 00:44:47 +0800

    fix: params parse bug

--- src/common/interceptors/transform.interceptor.ts
+++ src/common/interceptors/transform.interceptor.ts
@@ -7,6 +7,9 @@ import {
 import { map, Observable } from 'rxjs';
 import { ResOp } from '../model/response.model';
 import { BYPASS_KEY } from '../decorators/bypass.decorator';
+import * as qs from 'qs';
+import { FastifyRequest } from 'fastify';
+
 export class TransformInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     // 使用了 @Bypass() 跳过拦截器
@@ -14,6 +17,11 @@ export class TransformInterceptor implements NestInterceptor {
     if (bypass) {
       return next.handle();
     }
+
+    const http = context.switchToHttp();
+    const request = http.getRequest<FastifyRequest>();
+    // 处理 query 参数，将数组参数转换为数组,如：?a[]=1&a[]=2 => { a: [1, 2] }
+    request.query = qs.parse(request.url.split('?').at(1));
 
     return next.handle().pipe(
       map((data) => {
